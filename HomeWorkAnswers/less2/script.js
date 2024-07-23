315b108103ca732d3c9adfdfebc02a5451ccdab1
bee_olly <134434470+beolly@users.noreply.github.com>
2024-07-23 12:43:07 +0300

    Add files via upload

---
+++ HomeWorkAnswers/less2/script.js
@@ -0,0 +1,9 @@
+alert(new Date())
+var name=
+document.querySelector('#btn').onclick=function(){
+    let output=document.querySelector('#output')
+    // alert('Привіт {name}');
+    output.innerHTML="Hello, Dmytro";
+    output.style.color='blue';
+}
+
